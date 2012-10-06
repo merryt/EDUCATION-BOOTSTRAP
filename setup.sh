e4ab19bcd394ba8433218138ffd540f4fd792681
Dean Gullberry <teachhack5@gmail.com>
2012-10-06 14:53:13 -0600

    setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #
 
 echo Importing DB Schema and Development Data...
+mysql -uroot -proot -e"drop schema if exists edubootstrap; create schema edubootstrap"
 mysql -uroot -proot edubootstrap < db.sql
 echo Ok.
 echo
